8133c32fbd82553be0da982324fe0aaefdb4991f
MuhammadAK <94706207+Nearak@users.noreply.github.com>
2026-09-07 11:19:52 +0300

    Fix spelling of 'assets' in image paths
    Corrected the spelling of 'assets' in image paths.

--- game.js
+++ game.js
@@ -7,7 +7,7 @@
 /* ★ صورك الحقيقية: ضعها بجانب الملفات أو في مجلد assets/
    واكتب المسار هنا. فارغ = يُستخدم الرسم المدمج */
 const IMG={
-  camCar:'assets/car1.png',   // مثال: 'assets/cam04.jpg'
+  camCar:'assests/car1.png',   // مثال: 'assets/cam04.jpg'
   driver:'assests/person1p/png'    // مثال: 'assets/driver.jpg'
 };
 
