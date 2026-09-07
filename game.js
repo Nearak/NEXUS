d6de817f0566eb7e2b70f0e9500eb7d38e768490
MuhammadAK <94706207+Nearak@users.noreply.github.com>
2026-09-07 17:48:02 +0300

    Update video source URL for intro video

--- game.js
+++ game.js
@@ -1625,7 +1625,7 @@ function initIntro(){
   scr.hidden=false;
   playBtn.onclick=()=>{
     audioInit();
-    vid.src='assests/intro.mp4';
+    vid.src='https://res.cloudinary.com/rr0h5xww/video/upload/v1788782718/intro.mp4';
     playBtn.hidden=true;
     skipBtn.hidden=false;
     vid.play().catch(toBoot);
